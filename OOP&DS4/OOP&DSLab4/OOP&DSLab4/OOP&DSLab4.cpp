5d75c96add7cf1e9181527b0e5214c0773a84340
Mohamed Ehab Shalaby <107648111+Mido-Ehab@users.noreply.github.com>
2024-10-30 22:43:40 +0300

    Update OOP&DSLab4.cpp adding delete to the tree and adding menu manager

--- OOP&DS4/OOP&DSLab4/OOP&DSLab4/OOP&DSLab4.cpp
+++ OOP&DS4/OOP&DSLab4/OOP&DSLab4/OOP&DSLab4.cpp
@@ -1,211 +1,292 @@
+//-----------Binary Tree & queue Task ---------------------------------------------
+//---------------------------------------------------------------------
+#include <iostream>
+#include <string>
+using namespace std;
+
+//------Tree-----------------------------
+//---------------------------------------
+
+struct Student {
+	int id;
+	string name;
+	int grade;
+	Student* left = nullptr;
+	Student* right = nullptr;
+};
+
+struct StudentTree {
+	Student* root = nullptr;
+
+	void insertStudent(int id, string name, int grade) {
+		Student* newStudent = new Student{ id, name, grade };
+		if (root == nullptr) {
+			root = newStudent;
+		}
+		else {
+			Student* current = root;
+			Student* parent = nullptr;
+			while (current != nullptr) {
+				parent = current;
+				if (id < current->id) { //          ro7 3la elshemal lw el id 2a2al mn el leaf el e7na feha 
+					current = current->left;
+				}
+				else {                  //          ro7 3la elymeen lw el id 2a2al mn el leaf el e7na feha 
+					current = current->right;
+				}
+			}
+			if (id < parent->id) {
+				parent->left = newStudent;
+			}
+			else {
+				parent->right = newStudent;
+			}
+		}
+	}
+
+	Student* findStudentByID(int id) {
+		Student* current = root;
+		while (current != nullptr) {
+			if (current->id == id) {
+				return current;
+			}
+
+			///nfs klam el fo2 bs hena ba search 
+
+			else if (id < current->id) {
+				current = current->left;
+			}
+			else {
+				current = current->right;
+			}
+		}
+		return nullptr;
+	}
+
+	Student* getSuccessor(Student* curr)
+	{
+		curr = curr->right;
+		while (curr != NULL && curr->left != NULL)
+			curr = curr->left;
+		return curr;
+	}
+
+	Student* delNode(Student* root, int x)
+	{
+		if (root == nullptr) {
+			cout << "Tree is empty or not in the tree" << endl;
+			return root;
+		}
+
+		if (root->id > x)
+		{
+			root->left = delNode(root->left, x);
+		}
+		else if (root->id < x)
+		{
+			root->right = delNode(root->right, x);
+		}
+		else
+		{
+			// Cases when root has 0 children
+		  // or only right child
+			if (root->left == NULL) {
+				Student* temp = root->right;
+				delete root;
+				return temp;
+			}
+
+			// When root has only left child
+			if (root->right == NULL)
+			{
+				Student* temp = root->left;
+				delete root;
+				return temp;
+			}
+			Student* succ = getSuccessor(root);
+			root->id = succ->id;
+			root->right = delNode(root->right, succ->id);
+		}
+		return root;
+	}
+
+	void displayInOrder(Student* root) {
+		if (root != nullptr) {
+			displayInOrder(root->left);
+			cout << "Student: " << root->name << ", Grade: " << root->grade << endl;
+			displayInOrder(root->right);
+		}
+	}
+};
+
+
+//-------------------------------------------------------------
 //-------------Queue Task----------------------
-//#include <iostream>
-//#include<string>
-//using namespace std;
-//
-//struct Student {
-//	string name;
-//	int Grade;
-//	Student *next = nullptr;
-//};
-//
-//struct queue {
-//	Student* first = nullptr;
-//	Student* last = nullptr;
-//
-//	void enqueue(Student* newStud) {
-//		if ((first == nullptr) && (last == nullptr)) {
-//			last = newStud;
-//			first = newStud;
-//		}
-//		last->next = newStud;
-//		last = newStud;
-//	}
-//	Student* dequeue() {
-//		if (first == nullptr) {
-//			cout << "dequeue is null" << endl;
-//			return nullptr;
-//		}
-//		Student* temp = first;
-//		first = temp->next;
-//	}
-//
-//	Student* Search(int index) {
-//		if (first == nullptr) {
-//			cout << "Nothing here" << endl;
-//			return nullptr;
-//		}
-//		Student* current = first;
-//		int count = 0;
-//		while (current != nullptr && count < index) {
-//			current = current->next;
-//			count++;
-//		}
-//		if (current == nullptr) {
-//			cout << "Index out of range" << endl;
-//			return nullptr;
-//		}
-//		return current;
-//	}
-//
-//	void enqueueAt(Student* newStud, int index) {
-//		if (index == 0) {
-//			newStud->next = first;
-//			first = newStud;
-//			if (last == nullptr) {
-//				last = newStud;
-//			}
-//			return;
-//		}
-//		Student* prev = Search(index - 1);
-//		if (prev == nullptr) {
-//			cout << "Index out of range, cannot insert" << endl;
-//			return;
-//		}
-//		newStud->next = prev->next;
-//		prev->next = newStud;
-//		if (newStud->next == nullptr) {
-//			last = newStud;
-//		}
-//
-//	 }
-//
-//	};
-//
-//	int main()
-//	{
-//		queue q;
-//		Student s1 = { "Mohamed", 100 };
-//		Student s2 = { "Karim",60 };
-//		Student s3 = { "abdo",50 };
-//
-//		q.enqueue(&s1);
-//		q.enqueue(&s2);
-//		q.enqueue(&s3);
-//
-//		Student* Search = q.Search(2);
-//		if (Search != nullptr) {
-//			cout << "Peeking at index " << 2 << ": " << Search->name << " with grade " << Search->Grade << endl;
-//		}
-//		
-//		cout << "Before :" << endl;
-//		Student* curr = q.first;
-//		while (curr != nullptr) {
-//			cout << "Student: " << curr->name << ", Grade: " << curr->Grade << endl;
-//			curr = curr->next;
-//		}
-//
-//		// hkhly eluser el yktb meen el student we y7oto fe any index bs msh radya !!!??
-//		int index;
-//		string name;
-//		int grade;
-//		cout << "Enter the index for the new student ";
-//		cin >> index;
-//		cout << "new student name: ";
-//		cin >> name;
-//		cout << "grade : ";
-//		cin >> grade;
-//
-//		Student* newStudent = new Student{ name, grade };
-//		q.enqueueAt(newStudent, index);
-//
-//		cout << endl;
-//		cout << "After Updating  " << index << ":" << endl;
-//		curr = q.first;
-//		while (curr != nullptr) {
-//			cout << "Student: " << curr->name << ", Grade: " << curr->Grade << endl;
-//			curr = curr->next;
-//		}
-//
-//	}
+//----------------------------------------------
 
 
-//-----------Binary Tree Task---------------------------------------------
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//struct Student {
-//    int id;
-//    string name;
-//    int grade;
-//    Student* left = nullptr;
-//    Student* right = nullptr;
-//};
-//
-//struct StudentTree {
-//    Student* root = nullptr;
-//
-//    void insertStudent(int id, string name, int grade) {
-//        Student* newStudent = new Student{ id, name, grade };
-//        if (root == nullptr) {
-//            root = newStudent;
-//        }
-//        else {
-//            Student* current = root;
-//            Student* parent = nullptr;
-//            while (current != nullptr) {
-//                parent = current;
-//                if (id < current->id) { //          ro7 3la elshemal lw el id 2a2al mn el leaf el e7na feha 
-//                    current = current->left;
-//                }
-//                else {                  //          ro7 3la elymeen lw el id 2a2al mn el leaf el e7na feha 
-//                    current = current->right;
-//                }
-//            }
-//            if (id < parent->id) {
-//                parent->left = newStudent;
-//            }
-//            else {
-//                parent->right = newStudent;
-//            }
-//        }
-//    }
-//
-//    Student* findStudentByID(int id) {
-//        Student* current = root;
-//        while (current != nullptr) {
-//            if (current->id == id) {
-//                return current;
-//            }
-//
-//            ///nfs klam el fo2 bs hena ba search 
-//
-//            else if (id < current->id) {  
-//                current = current->left;
-//            }
-//            else {
-//                current = current->right;
-//            }
-//        }
-//        return nullptr;
-//    }
-//};
-//
-//int main() {
-//    StudentTree tree;
-//
-//    tree.insertStudent(1, "Mohamed", 90);
-//    tree.insertStudent(3, "Karim", 85);
-//    tree.insertStudent(2, "Abdo", 88);
-//    tree.insertStudent(5, "Salma", 95);
-//    tree.insertStudent(4, "abeer", 80);
-//
-//    int id;
-//    cout << "Search student by ID: ";
-//    cin >> id;
-//    Student* student = tree.findStudentByID(id);
-//    if (student != nullptr) {
-//        cout << "Found student: " << student->name << " with grade " << student->grade << endl;
-//    }
-//    else {
-//        cout << "Student with ID " << id << " not found." << endl;
-//    }
-//
-//    return 0;
-//}
+struct Studentq {
+	string name;
+	int Grade;
+	Studentq* next = nullptr;
+};
 
-//-----------------Stack Practice---------------------------------
+struct queue {
+	Studentq* first = nullptr;
+	Studentq* last = nullptr;
+
+	void enqueue(Studentq* newStud) {
+		if ((first == nullptr) && (last == nullptr)) {
+			last = newStud;
+			first = newStud;
+		}
+		last->next = newStud;
+		last = newStud;
+	}
+	Studentq* dequeue() {
+		if (first == nullptr) {
+			cout << "dequeue is null" << endl;
+			return nullptr;
+		}
+		Studentq* temp = first;
+		first = temp->next;
+	}
+
+	Studentq* Search(int index) {
+		if (first == nullptr) {
+			cout << "Nothing here" << endl;
+			return nullptr;
+		}
+		Studentq* current = first;
+		int count = 0;
+		while (current != nullptr && count < index) {
+			current = current->next;
+			count++;
+		}
+		if (current == nullptr) {
+			cout << "Index out of range" << endl;
+			return nullptr;
+		}
+		return current;
+	}
+
+	void enqueueAt(Studentq* newStud, int index) {
+		if (index == 0) {
+			newStud->next = first;
+			first = newStud;
+			if (last == nullptr) {
+				last = newStud;
+			}
+			return;
+		}
+		Studentq* prev = Search(index - 1);
+		if (prev == nullptr) {
+			cout << "Index out of range, cannot insert" << endl;
+			return;
+		}
+		newStud->next = prev->next;
+		prev->next = newStud;
+		if (newStud->next == nullptr) {
+			last = newStud;
+		}
+
+	}
+
+};
+
+int main() {
+	char con = NULL;
+	int choice;
+	StudentTree tree;
+	queue q;
+	do {
+		cout << "1. Tree operations\n";
+		cout << "2. Queue operations\n";
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+
+		switch (choice) {
+		case 1: {
+			tree.insertStudent(1, "Mohamed", 90);
+			tree.insertStudent(3, "Karim", 85);
+			tree.insertStudent(2, "Abdo", 88);
+			tree.insertStudent(5, "Salma", 95);
+			tree.insertStudent(4, "abeer", 80);
+
+			int id;
+			cout << "Search student by ID: ";
+			cin >> id;
+			Student* student = tree.findStudentByID(id);
+			if (student != nullptr) {
+				cout << "Found student: " << student->name << " with grade " << student->grade << endl;
+			}
+			else {
+				cout << "Student with ID " << id << " not found." << endl;
+			}
+
+
+			cout << "Enter the student ID to delete: ";
+			cin >> id;
+			tree.root = tree.delNode(tree.root, id);
+
+
+			cout << "Remaining students in the tree :" << endl;
+			tree.displayInOrder(tree.root);
+			break;
+		}
+		case 2: {
+			// queue code
+			Studentq s1 = { "Mohamed", 100 };
+			Studentq s2 = { "Karim",60 };
+			Studentq s3 = { "abdo",50 };
+
+			q.enqueue(&s1);
+			q.enqueue(&s2);
+			q.enqueue(&s3);
+
+			Studentq* Search = q.Search(2);
+			if (Search != nullptr) {
+				cout << "Peeking at index " << 2 << ": " << Search->name << " with grade " << Search->Grade << endl;
+			}
+
+			cout << "Before :" << endl;
+			Studentq* curr = q.first;
+			while (curr != nullptr) {
+				cout << "Student: " << curr->name << ", Grade: " << curr->Grade << endl;
+				curr = curr->next;
+			}
+
+			// hkhly eluser el yktb meen el student we y7oto fe any index bs msh radya !!!??
+			int index;
+			string name;
+			int grade;
+			cout << "Enter the index for the new student ";
+			cin >> index;
+			cout << "new student name: ";
+			cin >> name;
+			cout << "grade : ";
+			cin >> grade;
+
+			Studentq* newStudent = new Studentq{ name, grade };
+			q.enqueueAt(newStudent, index);
+
+			cout << endl;
+			cout << "After Updating  " << index << ":" << endl;
+			curr = q.first;
+			while (curr != nullptr) {
+				cout << "Student: " << curr->name << ", Grade: " << curr->Grade << endl;
+				curr = curr->next;
+			}
+		}
+		}
+		cout << "Press any key then enter to continue or q to quit : ";
+		cin >> con;
+	} while (con != 'q');
+	return 0;
+}
+
+
 //#include <iostream>
 //#include<string>
 //using namespace std;
@@ -355,3 +436,54 @@
 //		}
 //	}
 //}
+// 
+// 
+
+
+	//int main()
+	//{
+	//	queue q;
+	//	Studentq s1 = { "Mohamed", 100 };
+	//	Studentq s2 = { "Karim",60 };
+	//	Studentq s3 = { "abdo",50 };
+
+	//	q.enqueue(&s1);
+	//	q.enqueue(&s2);
+	//	q.enqueue(&s3);
+
+	//	Studentq* Search = q.Search(2);
+	//	if (Search != nullptr) {
+	//		cout << "Peeking at index " << 2 << ": " << Search->name << " with grade " << Search->Grade << endl;
+	//	}
+	//	
+	//	cout << "Before :" << endl;
+	//	Studentq* curr = q.first;
+	//	while (curr != nullptr) {
+	//		cout << "Student: " << curr->name << ", Grade: " << curr->Grade << endl;
+	//		curr = curr->next;
+	//	}
+
+	//	// hkhly eluser el yktb meen el student we y7oto fe any index bs msh radya !!!??
+	//	int index;
+	//	string name;
+	//	int grade;
+	//	cout << "Enter the index for the new student ";
+	//	cin >> index;
+	//	cout << "new student name: ";
+	//	cin >> name;
+	//	cout << "grade : ";
+	//	cin >> grade;
+
+	//	Studentq* newStudent = new Studentq{ name, grade };
+	//	q.enqueueAt(newStudent, index);
+
+	//	cout << endl;
+	//	cout << "After Updating  " << index << ":" << endl;
+	//	curr = q.first;
+	//	while (curr != nullptr) {
+	//		cout << "Student: " << curr->name << ", Grade: " << curr->Grade << endl;
+	//		curr = curr->next;
+	//	}
+
+	//}
+
