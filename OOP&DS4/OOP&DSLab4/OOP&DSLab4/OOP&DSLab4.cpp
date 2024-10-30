a9ce77b165a7388eb97b07f3954a41089e910e74
Mohamed Ehab Shalaby <107648111+Mido-Ehab@users.noreply.github.com>
2024-10-30 16:45:21 +0300

    Update OOP&DSLab4.cpp

--- OOP&DS4/OOP&DSLab4/OOP&DSLab4/OOP&DSLab4.cpp
+++ OOP&DS4/OOP&DSLab4/OOP&DSLab4/OOP&DSLab4.cpp
@@ -1,153 +1,3 @@
-//#include <iostream>
-//#include<string>
-//using namespace std;
-//
-//struct Node {
-//	string name;
-//	string address;
-//	Node *next = nullptr;
-//};
-//
-//struct stack {
-//	Node* top = nullptr;
-//	void push(Node* newNode)
-//	{
-//		if (top == nullptr)
-//		{
-//			top = newNode;
-//			return;
-//		}
-//		newNode->next = top;
-//		top = newNode;
-//		
-//	}
-//
-//
-//	Node* pop()
-//	{
-//		if (top == nullptr)
-//		{
-//			cout << "stack is empty";
-//			return NULL;
-//		}
-//		Node* temp;
-//		temp = top;
-//		top = top->next;
-//		return temp;
-//	}
-//
-//
-//	int peek() {
-//
-//	}
-//
-//
-//	void display() {
-//
-//
-//	}
-//
-//};
-//
-//
-//int main()
-//{
-//  {
-//	Node n1;
-//	Node n2;
-//	Node n3;
-//
-//	n1.name = "mohamed";
-//	n2.name = "Ehab";
-//	n3.name = "Third";
-//
-//	stack s;
-//
-//	s.push(&n1);
-//	s.push(&n2);
-//	s.push(&n3);
-//
-//	for (int i = 0; i < sizeof(s);i++) {
-//		Node* temp = s.pop();
-//		if (temp !=NULL)
-//		cout << "name: " <<(*temp).name << endl;
-//	}
-//   }
-//}
-
-//
-
-//-------------Stack With Linked List my edition------------
-//#include<iostream>
-//using namespace std;
-//
-//struct node {
-//	int data;
-//	node* next;
-//};
-//
-//node* top = NULL;
-//void display();
-//void push(int value);
-//int pop();
-//int peek();
-//
-//int main() {
-//	push(5);
-//	push(10);
-//	push(20);
-//	display();
-//	cout << "-------------------" << endl;
-//	pop();
-//	display();
-//	cout << "-------------------" << endl;
-//	peek();
-//}
-//
-//void push(int value) {
-//	node* new_node = new node;
-//	new_node->data = value;
-//	new_node->next = top;
-//	top = new_node;
-//}
-//
-//int pop() {
-//	int value;
-//	if (top == NULL) {
-//		cout << "stack is empty";
-//	}
-//	else {
-//		node* first_node = top;
-//		top = first_node->next;
-//		value = first_node->data;
-//		delete(first_node);
-//		return value;
-//	}
-//
-//
-//}
-//int peek() {
-//	if (top == NULL) {
-//		cout << "stack is empty";
-//	}
-//	else {
-//		return top->data;
-//	}
-//}
-//void display() {
-//	node* current;
-//	if (top == NULL) {
-//		cout << "stack is empty" << endl;
-//	}
-//	else {
-//		current = top;
-//		while (current != NULL) {
-//			cout << current->data << "\n";
-//			current = current->next;
-//		}
-//	}
-//}
-
 //-------------Queue Task----------------------
 //#include <iostream>
 //#include<string>
@@ -353,4 +203,155 @@
 //    }
 //
 //    return 0;
+//}
+
+//-----------------Stack Practice---------------------------------
+//#include <iostream>
+//#include<string>
+//using namespace std;
+//
+//struct Node {
+//	string name;
+//	string address;
+//	Node *next = nullptr;
+//};
+//
+//struct stack {
+//	Node* top = nullptr;
+//	void push(Node* newNode)
+//	{
+//		if (top == nullptr)
+//		{
+//			top = newNode;
+//			return;
+//		}
+//		newNode->next = top;
+//		top = newNode;
+//		
+//	}
+//
+//
+//	Node* pop()
+//	{
+//		if (top == nullptr)
+//		{
+//			cout << "stack is empty";
+//			return NULL;
+//		}
+//		Node* temp;
+//		temp = top;
+//		top = top->next;
+//		return temp;
+//	}
+//
+//
+//	int peek() {
+//
+//	}
+//
+//
+//	void display() {
+//
+//
+//	}
+//
+//};
+//
+//
+//int main()
+//{
+//  {
+//	Node n1;
+//	Node n2;
+//	Node n3;
+//
+//	n1.name = "mohamed";
+//	n2.name = "Ehab";
+//	n3.name = "Third";
+//
+//	stack s;
+//
+//	s.push(&n1);
+//	s.push(&n2);
+//	s.push(&n3);
+//
+//	for (int i = 0; i < sizeof(s);i++) {
+//		Node* temp = s.pop();
+//		if (temp !=NULL)
+//		cout << "name: " <<(*temp).name << endl;
+//	}
+//   }
+//}
+
+//
+
+//-------------Stack With Linked List my edition------------
+//#include<iostream>
+//using namespace std;
+//
+//struct node {
+//	int data;
+//	node* next;
+//};
+//
+//node* top = NULL;
+//void display();
+//void push(int value);
+//int pop();
+//int peek();
+//
+//int main() {
+//	push(5);
+//	push(10);
+//	push(20);
+//	display();
+//	cout << "-------------------" << endl;
+//	pop();
+//	display();
+//	cout << "-------------------" << endl;
+//	peek();
+//}
+//
+//void push(int value) {
+//	node* new_node = new node;
+//	new_node->data = value;
+//	new_node->next = top;
+//	top = new_node;
+//}
+//
+//int pop() {
+//	int value;
+//	if (top == NULL) {
+//		cout << "stack is empty";
+//	}
+//	else {
+//		node* first_node = top;
+//		top = first_node->next;
+//		value = first_node->data;
+//		delete(first_node);
+//		return value;
+//	}
+//
+//
+//}
+//int peek() {
+//	if (top == NULL) {
+//		cout << "stack is empty";
+//	}
+//	else {
+//		return top->data;
+//	}
+//}
+//void display() {
+//	node* current;
+//	if (top == NULL) {
+//		cout << "stack is empty" << endl;
+//	}
+//	else {
+//		current = top;
+//		while (current != NULL) {
+//			cout << current->data << "\n";
+//			current = current->next;
+//		}
+//	}
 //}
